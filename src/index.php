261cd4c3c6dce43ea12aec58ad54c50139a50514
Michael Everett <micahel.everett@curology.com>
2022-01-12 16:14:43 -0800

    foo

--- src/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-echo "I am Michael's 2nd attempt at writing PHP. <br>";
-echo '<img src="https://www.looper.com/img/gallery/robocop-sequel-loses-director-neill-blomkamp/intro-1565879277.jpg">';
-
-?>
